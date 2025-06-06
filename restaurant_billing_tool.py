74843b219bee1339b8c3dc64ead5f13b99df63e1
Roshan-7718 <roshanrajarithish@gmail.com>
2025-06-07 00:23:55 +0530

    Add files via upload

---
+++ restaurant_billing_tool.py
@@ -0,0 +1,15 @@
+lunch_items = ["Paneer Butter Masala", "Butter Naan", "Lassi", "Gulab Jamun", "Jeera Rice"]
+print("Ordered Items:", lunch_items)
+
+print("\n")
+
+print("Total number of items ordered:", len(lunch_items))
+
+print("\n")
+
+lunch_items.append("Papad")
+print("Updated Ordered Items:", lunch_items)
+
+print("\n")
+
+print("Updated total number of items ordered:", len(lunch_items))
